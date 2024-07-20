5e53612bde91f2279f707704ce690f5d8d95fe20
afride596 <afride596@gmail.com>
2024-07-20 23:37:24 +0530

    completed dataTypes

---
+++ javascript/DataTypes.js
@@ -0,0 +1,21 @@
+"Use strict";// treat all js code as newer version
+
+let name="afride"
+let age=22
+let isLoggedIn=true
+console.table([name,age,isLoggedIn])
+
+// primtive dataType
+
+// BigInt
+// String =>"";
+// boolean => true/flase
+// null=>standalone value 
+// undefined=>not defined 
+// symbol => unique 
+
+// non-primitive dataTypes
+ 
+// Array=[]
+// Object={}
+// Function=>function(){}
---
+++ javascript/LetConst.js
@@ -0,0 +1,18 @@
+const accountId=17526426;
+let Email="mohammadafride2003@gmail.com"
+var accountPassword="8798"
+let accountCity="vijayawada";
+
+let accountstatment;
+
+function x(){
+
+    let Email='Afride@gmail.com';
+    console.log(Email);
+
+}
+console.log(accountId);
+console.log(Email);
+console.log(accountPassword);
+console.log(accountCity);
+x()
